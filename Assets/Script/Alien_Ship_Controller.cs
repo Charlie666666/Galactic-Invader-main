abaa72bfe0c8c7afd25b6baa930a8c555f03934f
Charlie666666 <71334200+Charlie666666@users.noreply.github.com>
2023-02-23 22:36:22 -0500

    Big Updates
    Victory Screens for all players, added the code for Quit and Restart Buttons and fixed all the UI bugs

--- Assets/Script/Alien_Ship_Controller.cs
+++ Assets/Script/Alien_Ship_Controller.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-using UnityEngine;
 
 public class Alien_Ship_Controller : MonoBehaviour
 {
---
+++ Assets/Script/RestartButton_H.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartButton_H : MonoBehaviour
+{
+    void Start()
+    {
+        // Find the Button component attached to this GameObject
+        Button restartButton = GetComponent<Button>();
+        
+        // Add an onclick listener to the Button component, and specify the method to call when the button is clicked
+        restartButton.onClick.AddListener(RestartGame);
+    }
+
+    void RestartGame()
+    {
+        // Change the scene to MainMenu
+        SceneManager.LoadScene("MainMenu");
+    }
+}
