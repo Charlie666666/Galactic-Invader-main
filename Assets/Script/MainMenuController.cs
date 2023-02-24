62af43d3140c1d0d56d93b3a424908f9f0202f79
Charlie666666 <71334200+Charlie666666@users.noreply.github.com>
2023-02-24 12:44:43 -0500

    Update
    BG Musics

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
-    
+    public AudioClip ButtonClickEffect;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Get the AudioSource component on this game object
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        // Play the button click sound effect
+        audioSource.PlayOneShot(ButtonClickEffect);
+    }
     public void PlayGame() {
         
         SceneManager.LoadScene("SampleScene");
     }
     
-        
-   
+    
 }
